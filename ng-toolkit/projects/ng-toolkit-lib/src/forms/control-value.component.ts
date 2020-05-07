fcdd43bb2b9e8d9342be79c0e6754bb4c89ac952
Martin Bojnansky <martin.bojnansky@paranor.ch>
2020-05-08 01:38:14 +0200

    Adds controlValueComponent

---
+++ ng-toolkit/projects/ng-toolkit-lib/src/forms/control-value.component.ts
@@ -0,0 +1,47 @@
+import { ChangeDetectorRef } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
+
+export abstract class ControlValueComponent<T> implements ControlValueAccessor {
+  get value(): T {
+    return this._value;
+  }
+
+  get disabled() {
+    return this._disabled;
+  }
+
+  writeValue(value: T) {
+    if (this._value !== value) {
+      this._value = value;
+      this.onChange(value);
+    }
+
+    this.changeDetectorRef.markForCheck();
+  }
+
+  registerOnChange(fn: (value: T) => void) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void) {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    if (this._disabled !== isDisabled) {
+      this._disabled = isDisabled;
+    }
+
+    this.changeDetectorRef.markForCheck();
+  }
+
+  // tslint:disable-next-line: variable-name
+  protected _value: T;
+  // tslint:disable-next-line: variable-name
+  protected _disabled: boolean;
+
+  protected constructor(protected changeDetectorRef: ChangeDetectorRef) {}
+
+  protected onChange: (value: T) => void = () => {};
+  protected onTouched: () => void = () => {};
+}
