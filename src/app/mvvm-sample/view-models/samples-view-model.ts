abf7b8399596e7842585050e149934411af1b868
Martin Bojnansky <martin.bojnansky@paranor.ch>
2021-03-24 18:40:22 +0100

    Adds synced form controls

--- src/app/mvvm-sample/view-models/samples-view-model.ts
+++ src/app/mvvm-sample/view-models/samples-view-model.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ViewModel } from 'dist/ng-toolkit-lib';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class SamplesViewModel extends ViewModel {
   editable$ = new BehaviorSubject<boolean>(true);
+
+  form = new FormGroup({
+    editable: new FormControl(),
+    text: new FormControl('intial text'),
+  });
 
   constructor() {
     super();
